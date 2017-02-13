ac1c4c061477ef0b662a4d886bb363c965db4021
Benjamin Adams <armastevs@gmail.com>
2017-02-13 11:44:53 -0600

    added roku api

---
+++ roku-api.php
@@ -0,0 +1,221 @@
+[{
+    "url": "http:\/\/cutecaptions.com\/img\/look-at-my-cute-pink-nose.jpg",
+    "title": "LOOK AT MY CUTE, PINK NOSE",
+    "width": 554,
+    "height": 571
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/world-domination.jpeg",
+    "title": "World Domination",
+    "width": 650,
+    "height": 497
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/same-thing-happens-with-cats.png",
+    "title": "Same Thing Happens with Cats",
+    "width": 538,
+    "height": 333
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/dis-is-mah-breakfast-right.jpeg",
+    "title": "Dis is Mah Breakfast, Right?",
+    "width": 650,
+    "height": 346
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/go-awae.jpeg",
+    "title": "Go Awae",
+    "width": 640,
+    "height": 499
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/i-can-haz-a-cheezburger-.jpeg",
+    "title": "I Can Haz a Cheezburger ?",
+    "width": 500,
+    "height": 441
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/phat-the-cat.jpeg",
+    "title": "Phat the Cat",
+    "width": 650,
+    "height": 490
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/i-miss-you-so-much-it-hurts.jpg",
+    "title": "I Miss You So Much, It Hurts",
+    "width": 542,
+    "height": 382
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/exquisite-cat-is-exquisite.jpeg",
+    "title": "Exquisite Cat is Exquisite",
+    "width": 500,
+    "height": 419
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/you-make-kitty-scared.jpeg",
+    "title": "You Make Kitty Scared!",
+    "width": 405,
+    "height": 454
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/ive-waited.jpeg",
+    "title": "I've Waited...",
+    "width": 500,
+    "height": 393
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/the-first-lolcat-i-ever-saw.jpeg",
+    "title": "The First Lolcat I Ever Saw",
+    "width": 599,
+    "height": 608
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/cant-sleep-clownll-eat-me.jpeg",
+    "title": "Can't Sleep, Clown'll Eat Me!",
+    "width": 648,
+    "height": 493
+}, {
+    "url": "http:\/\/cutecaptions.com\/images\/jungle-cat\/what-do-you-mean-im-not-a-leopard.jpg",
+    "title": "What Do You Mean, I'm Not a Leopard!?!?",
+    "width": 398,
+    "height": 549
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/are-you-kidding-me-you-expected-me.jpg",
+    "title": "Are You Kidding Me?, You Expected Me to Sleep on the Floor? Ha!",
+    "width": 540,
+    "height": 423
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/mom-slow-down-my-legs-are-short.jpg",
+    "title": "Mom!!!, Slow Down, My Legs Are Short!!!",
+    "width": 500,
+    "height": 378
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/comic-sans.jpeg",
+    "title": "Comic Sans",
+    "width": 650,
+    "height": 498
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/hay-guise.jpeg",
+    "title": "Hay Guise!!",
+    "width": 604,
+    "height": 463
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/sums-up-every-animal-out-there.jpeg",
+    "title": "Sums Up Every Animal out There.",
+    "width": 500,
+    "height": 359
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/kitty-kleenex.jpeg",
+    "title": "Kitty Kleenex",
+    "width": 515,
+    "height": 393
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/i-swears-twasent-meh.jpeg",
+    "title": "I Swears Twasent Meh.",
+    "width": 560,
+    "height": 287
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/soon-xpost-from-adviceanimals.jpeg",
+    "title": "Soon (x-post from Adviceanimals)",
+    "width": 650,
+    "height": 494
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/step-right-up-get-your-kisses-only.jpg",
+    "title": "Step Right Up! Get Your Kisses!, Only 5 Milkbones!",
+    "width": 553,
+    "height": 433
+}, {
+    "url": "http:\/\/cutecaptions.com\/images\/ninja-attack-cat\/everybody-was-kung-fu-fighting.jpg",
+    "title": "Everybody Was, Kung Fu Fighting",
+    "width": 540,
+    "height": 558
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/the-dog-did-it-i-was-just-looking.jpg",
+    "title": "The Dog Did It, I Was Just Looking at the Mess He Made",
+    "width": 553,
+    "height": 432
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/spoiled-kittah-takes-naps-all-day.jpg",
+    "title": "Spoiled Kittah, Takes Naps All Day",
+    "width": 556,
+    "height": 435
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/choo-choo-all-kittens-on-board-the.jpg",
+    "title": "Choo Choo!, All Kittens on Board the Sleppy Time Train!!",
+    "width": 540,
+    "height": 411
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/unbelievable.jpeg",
+    "title": "Unbelievable...",
+    "width": 400,
+    "height": 308
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/what-have-i-done-with-my-life.jpeg",
+    "title": "What Have I Done with My Life?",
+    "width": 530,
+    "height": 407
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/look-at-it.jpeg",
+    "title": "Look at It!",
+    "width": 640,
+    "height": 498
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/prepare-to-die.jpeg",
+    "title": "Prepare to Die",
+    "width": 500,
+    "height": 393
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/rought-night.jpeg",
+    "title": "Rought Night",
+    "width": 650,
+    "height": 398
+}, {
+    "url": "http:\/\/cutecaptions.com\/images\/sweet\/gimme-a-smile.jpg",
+    "title": "Gimme a Smile!",
+    "width": 541,
+    "height": 423
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/patience.png",
+    "title": "Patience",
+    "width": 490,
+    "height": 347
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/i-bathed-u-while-u-slept.jpeg",
+    "title": "I Bathed U While U Slept",
+    "width": 460,
+    "height": 627
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/april-fools-is-my-eldest-cats-birthday-she-turned-15-and-she-is-quite-a-grouch-about-being-older.jpeg",
+    "title": "April Fool's is My Eldest Cat's Birthday, She Turned 15, and She is Quite a Grouch About Being Older!",
+    "width": 497,
+    "height": 391
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/with-our-powers-combined.png",
+    "title": "With Our Powers Combined...",
+    "width": 640,
+    "height": 490
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/whats-the-point-of-buying-these-do.jpg",
+    "title": "Whats the Point of Buying These Dog Beds!",
+    "width": 463,
+    "height": 568
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/plastic-bag-cat-the-sequel.jpeg",
+    "title": "Plastic Bag Cat: the Sequel",
+    "width": 650,
+    "height": 498
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/no-jerry-no-hes-not-worth-it.jpg",
+    "title": "No Jerry No, Hes Not Worth It!!!!",
+    "width": 540,
+    "height": 422
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/ermahgerd-cat.jpeg",
+    "title": "Ermahgerd Cat",
+    "width": 650,
+    "height": 506
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/bah-humbug-cat-reduc.jpeg",
+    "title": "Bah Humbug Cat Reduc",
+    "width": 650,
+    "height": 506
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/need-moor-shooz.jpeg",
+    "title": "Need Moor Shooz!",
+    "width": 650,
+    "height": 384
+}, {
+    "url": "http:\/\/cutecaptions.com\/img\/jailhouse-confidential.jpeg",
+    "title": "Jailhouse Confidential",
+    "width": 500,
+    "height": 466
+}]
